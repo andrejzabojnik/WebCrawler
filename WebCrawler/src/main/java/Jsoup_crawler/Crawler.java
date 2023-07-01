dd958b4d452b6d399ba5d6cce422c14c1f323c47
Matej Bujdák <124814116+MatejBujdak@users.noreply.github.com>
2023-07-01 18:47:22 +0200

    fix(output): Odstranenie nepotrebnych veci

--- WebCrawler/src/main/java/Jsoup_crawler/Crawler.java
+++ WebCrawler/src/main/java/Jsoup_crawler/Crawler.java
@@ -4,8 +4,6 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import com.cybozu.labs.langdetect.DetectorFactory;
-import com.cybozu.labs.langdetect.LangDetectException;
 
 
 import java.io.IOException;
