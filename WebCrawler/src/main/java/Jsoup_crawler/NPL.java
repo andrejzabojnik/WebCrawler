80a9e23d1a8b1ded487f5e6a8ac0967fcfdc22fd
Matej Bujdák <124814116+MatejBujdak@users.noreply.github.com>
2023-07-05 16:25:32 +0200

    feat: pridanie kniznice morphodita

--- WebCrawler/src/main/java/Jsoup_crawler/NPL.java
+++ WebCrawler/src/main/java/Jsoup_crawler/NPL.java
@@ -2,12 +2,14 @@ package Jsoup_crawler;
 
 import sk.textprocessor.processing.TextProcesses;
 
+
+
 import java.util.Arrays;
 
 public class NPL {
     static String textProcessing(String text){
         TextProcesses textProcesses = new TextProcesses();
-        String[] output = textProcesses.tokenize(text);
+        String[] output = textProcesses.lemmatize(text);
 
         return Arrays.toString(output);
     }
