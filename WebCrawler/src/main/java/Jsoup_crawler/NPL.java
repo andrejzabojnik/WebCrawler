209b5bc68f29cda2feb53bebef1a56b31a4986e0
Matej Bujdák <124814116+MatejBujdak@users.noreply.github.com>
2023-07-08 11:46:59 +0200

    feat: pridanie importov

--- WebCrawler/src/main/java/Jsoup_crawler/NPL.java
+++ WebCrawler/src/main/java/Jsoup_crawler/NPL.java
@@ -1,15 +1,15 @@
 package Jsoup_crawler;
 
 import sk.textprocessor.processing.TextProcesses;
-
-
-
 import java.util.Arrays;
+
 
 public class NPL {
     static String textProcessing(String text){
         TextProcesses textProcesses = new TextProcesses();
+
         String[] output = textProcesses.lemmatize(text);
+
 
         return Arrays.toString(output);
     }
